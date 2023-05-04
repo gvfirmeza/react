837ab0c0d2c9f419c3a0ae2eafbf37fe8511cb0c
mncbl <mateusncbl@gmail.com>
2023-05-04 11:34:33 -0300

    tela Cadastro

--- react-app/src/pages/Cadastro1.jsx
+++ react-app/src/pages/Cadastro1.jsx
@@ -1,12 +1,54 @@
 import Menu from "../components/Menu/Menu";
 import Cartao from "../components/Cartao/Cartao";
+import { DivCadastro } from "./style_cadastro";
 
 const Cadastro1 = () => (
-    <>
+  <>
     <Menu />
+    <center>
     <h1>Cadastro de novo usuario</h1>
+    </center>
+    <center>
+      <DivCadastro>
+        <form>
+          <label for="Nome">Digite seu Nome</label>
+          <p>
+            <input type="text" placeholder="name" name="name" />
+          </p>
+          <label for="Sobrenomeltimo Nome">Digite seu Sobrenome</label>
+          <p>
+            <input type="text" placeholder="sobrenome" name="sobrenome" />
+          </p>
+          <label for="senha">Digite sua Senha</label>
+          <p>
+            <input type="password" placeholder="senha" name="senha" />
+          </p>
+          <label for=" Confirme senha">Confirme sua Senha</label>
+          <p>
+            <input
+              type="password"
+              placeholder="Confirme sua senha"
+              name="senha"
+            />
+          </p>
+          <label for="email">Digite seu Email</label>
+          <p>
+            <input type="email" placeholder="email" name="email" />
+          </p>
+          <label for="Setor">Selecione o setor</label>
 
-    </>
-)
+          <p><select>
+            <option value="ADM">ADM</option>
+            <option value="COMERCIAL">COMERCIAL</option>
+            <option value="TECNOLOGIA">TECNOLOGIA</option>
+          </select>
+          </p>
 
-export default Cadastro1 ;
+          <button type="submit">CADASTRAR</button>
+        </form>
+      </DivCadastro>
+    </center>
+  </>
+);
+
+export default Cadastro1;
---
+++ react-app/src/pages/style_cadastro.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+const DivCadastro = styled.div ` 
+    flex-direction: column;
+    font-size:20px;
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 2em;
+    text-align:center;
+    border-radius: 15px;
+    width: fit-content;
+    background-color: rgb(51,110,255);
+    display: flex ;
+    align-items: center;
+
+
+    a {
+        text-decoration: none;
+    }
+
+`;
+
+export { DivCadastro };
