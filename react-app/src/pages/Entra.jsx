ecad3d3debcff1a75a5c52fc7f49a34ab9c800fa
GUSTAVO AZEVEDO RAIA DE SIQUEIRA <202201037385@alunos.ibmec.edu.br>
2023-05-04 11:34:20 -0300

    Tela Login

--- react-app/src/pages/Entra.jsx
+++ react-app/src/pages/Entra.jsx
@@ -1,17 +1,17 @@
 import Menu from '../components/Menu/Menu';
-import { DivPrincipal } from './style_entra';
+import { Botao, DivPrincipal, Input } from './style_entra';
 
 function Entra() {
   return (
     <div className="Entra">
       <Menu />
       <header className="App-header">
-        <h1>Faça seu Login</h1>
         <DivPrincipal>
             <label for="email">Digite seu E-mail</label>
-            <p><input type="text" placeholder='email' name="email"/></p>
+            <p><Input type="e-mail" placeholder=' email' name="email"/></p>
             <label for="senha">Digite sua Senha</label>
-            <p><input type="text" placeholder='senha' name="senha"/></p>
+            <p><Input type="password" placeholder=' senha' name="senha"/></p>
+            <Botao type='button'>Logar</Botao>
         </DivPrincipal>
       </header>
     </div>
--- react-app/src/pages/style_entra.js
+++ react-app/src/pages/style_entra.js
@@ -1,14 +1,39 @@
 import styled from "styled-components";
 
 const DivPrincipal = styled.div ` 
-    flex-direction: row;
-    gap: 2em;
-    background-color: gray;
+    /* gap: 1em; */
+    color: white;
+    font-weight: bolder;
+    background: blue;
+    background: linear-gradient(90deg, blue 35%, darkblue 100%);
+    padding: 40pt;
+    width: 28rem;
+    margin: 0 auto;
+    margin-top: 100pt;
+    border-radius: 20px;
 
     a {
         text-decoration: none;
     }
-
 `;
 
-export { DivPrincipal };
+const Input = styled.input`
+    border: 1px solid black;
+    border-radius: 25px;
+    height: 30px;
+    width: 25em;
+    padding-left: 15px;
+    font-size: 12pt;
+`;
+
+const Botao = styled.button`
+    margin-top: 3em;
+    height: 40px;
+    width: 12em;
+    background: black;
+    color: white;
+    border: 1px solid white;
+    border-radius: 40px;
+`;
+
+export { DivPrincipal, Input, Botao };
