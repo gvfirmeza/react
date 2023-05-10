f0e081271273dc3359c00ff7f5996e3d180e8275
GUSTAVO AZEVEDO RAIA DE SIQUEIRA <202201037385@alunos.ibmec.edu.br>
2023-05-10 11:16:17 -0300

    Entra Fundo

--- react-app/src/pages/Entra.jsx
+++ react-app/src/pages/Entra.jsx
@@ -1,19 +1,23 @@
 import Menu from '../components/Menu/Menu';
 import { Botao, DivPrincipal, Input } from './style_entra';
+import { Titulo, Fundo } from './style';
 
 function Entra() {
   return (
     <div className="Entra">
-      <Menu />
-      <header className="App-header">
-        <DivPrincipal>
+      <Fundo>
+        <Menu />
+        <header className="App-header">
+          <DivPrincipal>
             <label for="email">Digite seu E-mail</label>
-            <p><Input type="e-mail" placeholder=' email' name="email"/></p>
+            <p><Input type="e-mail" placeholder=' email' name="email" /></p>
             <label for="senha">Digite sua Senha</label>
-            <p><Input type="password" placeholder=' senha' name="senha"/></p>
+            <p><Input type="password" placeholder=' senha' name="senha" /></p>
             <Botao type='button'>Logar</Botao>
-        </DivPrincipal>
-      </header>
+          </DivPrincipal>
+        </header>
+      </Fundo>
+
     </div>
   );
 }
